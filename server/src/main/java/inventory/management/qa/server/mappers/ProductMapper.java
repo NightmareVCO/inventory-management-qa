19d07a5011a6d5dd2d52071253e8c0d1c13e3c67
NightmareVCO <vladimircuriel@outlook.com>
2025-08-17 02:53:44 -0400

    style: reorganize imports and format ProductMapper for improved readability

--- server/src/main/java/inventory/management/qa/server/mappers/ProductMapper.java
+++ server/src/main/java/inventory/management/qa/server/mappers/ProductMapper.java
@@ -1,24 +1,28 @@
+/* (C)2025 */
 package inventory.management.qa.server.mappers;
 
-import java.util.List;
-
+import inventory.management.qa.server.dtos.ProductRequestDTO;
+import inventory.management.qa.server.dtos.ProductResponseDTO;
+import inventory.management.qa.server.dtos.ProductStockResponseDTO;
 import inventory.management.qa.server.dtos.ReducedProductResponseDTO;
+import inventory.management.qa.server.entities.Product;
+import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-import inventory.management.qa.server.dtos.ProductRequestDTO;
-import inventory.management.qa.server.dtos.ProductResponseDTO;
-import inventory.management.qa.server.entities.Product;
-
 @Mapper
 public interface ProductMapper {
-  ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
+    ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
 
-  @Mapping(target = "id", ignore = true)
-  Product productRequestDTOToProduct(ProductRequestDTO productRequestDTO);
-  ProductResponseDTO productToProductResponseDTO(Product product);
+    @Mapping(target = "id", ignore = true)
+    Product productRequestDTOToProduct(ProductRequestDTO productRequestDTO);
 
-  List<ProductResponseDTO> productsToProductResponseDTOs(List<Product> products);
-  List<ReducedProductResponseDTO> productsToReducedProductResponseDTO(List<Product> products);
+    ProductResponseDTO productToProductResponseDTO(Product product);
+
+    List<ProductResponseDTO> productsToProductResponseDTOs(List<Product> products);
+
+    List<ReducedProductResponseDTO> productsToReducedProductResponseDTO(List<Product> products);
+
+    ProductStockResponseDTO productStockToProductStockResponseDTO(Product product);
 }

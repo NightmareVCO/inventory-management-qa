1fe1b8c8b220839a24d641d957a88291aa97908f
NightmareVCO <vladimircuriel@outlook.com>
2025-08-17 02:44:46 -0400

    feat: extract useFilterByMinAmount hook for managing minimum price filtering logic

--- client/src/components/filters/FilterByMinAmount.tsx
+++ client/src/components/filters/FilterByMinAmount.tsx
@@ -9,9 +9,7 @@ import {
 	NumberInputField,
 	NumberInputStepper,
 } from '@chakra-ui/react';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import { useDebouncedCallback } from 'use-debounce';
+import useFilterByMinAmount from '@/lib/hooks/useFilterByMinAmount';
 
 interface FilterByMinAmountProps {
 	label?: string;
@@ -20,32 +18,7 @@ interface FilterByMinAmountProps {
 export default function FilterByMinAmount({
 	label = 'Min Price',
 }: FilterByMinAmountProps) {
-	const searchParams = useSearchParams();
-	const { replace } = useRouter();
-	const pathname = usePathname();
-
-	const [value, setValue] = useState(searchParams.get('minPrice') || '');
-
-	useEffect(() => {
-		setValue(searchParams.get('minPrice') || '');
-	}, [searchParams]);
-
-	const updateURL = useDebouncedCallback((newValue: string) => {
-		const params = new URLSearchParams(searchParams);
-
-		if (newValue && Number.parseFloat(newValue) > 0) {
-			params.set('minPrice', newValue);
-		} else {
-			params.delete('minPrice');
-		}
-
-		replace(`${pathname}?${params.toString()}`);
-	}, 500);
-
-	const handleChange = (valueString: string) => {
-		setValue(valueString);
-		updateURL(valueString);
-	};
+	const { value, handleChange } = useFilterByMinAmount();
 
 	return (
 		<FormControl>

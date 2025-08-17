479c6ce9918593fb584efad2fbd4f5f60df8e8f1
NightmareVCO <vladimircuriel@outlook.com>
2025-08-17 02:43:26 -0400

    feat: implement ProductStockControllerCard and useProductStockControllerCard for stock management

---
+++ client/src/components/card/ProductStockControllerCard.tsx
@@ -0,0 +1,111 @@
+import { Box, Button, Flex, Icon, Text, useToast } from '@chakra-ui/react';
+import { useInventoryPage } from '@lib/hooks/useInventoryPage';
+import useProductStockControllerCard from '@lib/hooks/useProductStockControllerCard';
+import { useKeycloak } from '@react-keycloak/web';
+import { FiBox } from 'react-icons/fi';
+import type { Product } from '@/lib/model/product.model';
+
+export type ProductStockControllerCardProps = {
+	product: Product;
+};
+
+export default function ProductStockControllerCard({
+	product,
+}: ProductStockControllerCardProps) {
+	const toast = useToast();
+	const { keycloak } = useKeycloak();
+	const { refreshProducts } = useInventoryPage();
+	const { quantity, handleIncrease, handleDecrease } =
+		useProductStockControllerCard({
+			product,
+			token: keycloak.token ?? '',
+			onSuccess: (product) => {
+				toast({
+					title: 'Stock updated',
+					description: `${product.name} stock has been updated to ${quantity} from ${product.quantity}`,
+					status: quantity < +product.quantity ? 'warning' : 'success',
+					duration: 5000,
+					isClosable: true,
+				});
+
+				refreshProducts();
+			},
+			onError: (error) => {
+				toast({
+					title: 'Stock update failed',
+					description: error.message,
+					status: 'error',
+					duration: 5000,
+					isClosable: true,
+				});
+			},
+		});
+
+	return (
+		<Flex
+			p={1}
+			w="fit"
+			minW="350px"
+			alignItems="center"
+			justifyContent="center"
+		>
+			<Box
+				bg="white"
+				w="100%"
+				borderWidth="1px"
+				rounded="lg"
+				shadow="sm"
+				position="relative"
+				overflow="hidden"
+			>
+				<Box p={4}>
+					<Flex justifyContent="space-between" alignItems="center">
+						<Flex px={4} alignItems="center">
+							<Icon as={FiBox} h={5} w={5} color="turquoise.800" mr={3} />
+							<Text
+								fontSize="md"
+								fontWeight="semibold"
+								lineHeight="tight"
+								noOfLines={1}
+							>
+								{product.name}
+							</Text>
+						</Flex>
+						<Flex px={4} alignItems="center" gap={2}>
+							<Button
+								color={'red.500'}
+								bg="red.100"
+								_hover={{ bg: 'red.200' }}
+								onClick={handleDecrease}
+							>
+								<Text fontSize="sm" fontWeight="bold">
+									-
+								</Text>
+							</Button>
+							<Box
+								px={4}
+								py={1}
+								borderWidth="1px"
+								borderRadius="md"
+								textAlign="center"
+								fontWeight="semibold"
+							>
+								{quantity}
+							</Box>
+							<Button
+								color={'green.500'}
+								bg="green.100"
+								_hover={{ bg: 'green.200' }}
+								onClick={handleIncrease}
+							>
+								<Text fontSize="sm" fontWeight="bold">
+									+
+								</Text>
+							</Button>
+						</Flex>
+					</Flex>
+				</Box>
+			</Box>
+		</Flex>
+	);
+}

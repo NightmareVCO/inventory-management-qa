f56f7c2262c7384184ccf403035987344df2a75e
NightmareVCO <vladimircuriel@outlook.com>
2025-08-17 02:50:07 -0400

    fix: standardize variable names for permission files in authentication tests

--- client/tests/auth.setup.ts
+++ client/tests/auth.setup.ts
@@ -1,7 +1,7 @@
 import { expect, test as setup } from '@playwright/test';
 import path from 'path';
 
-const authFile = path.join(__dirname, '../playwright/.auth/user.json');
+const PERM_FILE = path.join(__dirname, '../playwright/.auth/user.json');
 
 const LOGIN_PAGE =
 	'http://localhost:7080/realms/inventory-realm/protocol/openid-connect/auth?client_id=inventory-frontend&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&state=f0bf7df6-303e-4398-af50-0b6a566cba77&response_mode=fragment&response_type=code&scope=openid&nonce=8fc417cb-f39f-4e11-ab78-e45217d6a7df&code_challenge=jM6XGojdokuccskDzkeDM0R3sYRGtjIvntofAr48LwU&code_challenge_method=S256';
@@ -24,5 +24,5 @@ setup('authenticate with Keycloak', async ({ page }) => {
 		page.locator('button:has-text("Go to Inventory")'),
 	).toBeVisible();
 
-	await page.context().storageState({ path: authFile });
+	await page.context().storageState({ path: PERM_FILE });
 });

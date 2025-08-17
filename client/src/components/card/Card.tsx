2c7949c8b31e80ffa815a66ad8b1e22513960859
NightmareVCO <vladimircuriel@outlook.com>
2025-08-17 02:42:39 -0400

    refactor: remove unused import from Card.tsx

--- client/src/components/card/Card.tsx
+++ client/src/components/card/Card.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { Box, Flex, Icon, Text, useColorModeValue } from '@chakra-ui/react';
-import { px } from 'framer-motion';
 import { FiBox } from 'react-icons/fi';
 
 interface ProductCardProps {

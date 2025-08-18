e4c26404b5dca470b8bc15760771103b090baa68
NightmareVCO <vladimircuriel@outlook.com>
2025-08-18 01:19:31 -0400

    feat: create ChangesPage component and useChangesPage hook for displaying product changes

---
+++ client/src/app/changes/page.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { Accordion, Flex } from '@chakra-ui/react';
+import AccordionChange from '@components/accordion/ChangesAccordion';
+import LoadingScreen from '@components/loading/LoadingScreen';
+import SidebarWithHeader from '@components/navigation/sidebar/SidebarWithBanner';
+import Pagination from '@components/pagination/Pagination';
+import { useChangesPage } from '@lib/hooks/useChangesPage';
+import type { ProductChanges } from '@/lib/model/productChanges.model';
+
+export default function ChangesPage() {
+	const { changes, isLoading, isAuthChecking } = useChangesPage();
+
+	if (isAuthChecking || isLoading) {
+		return <LoadingScreen />;
+	}
+
+	console.log('Changes:', changes);
+
+	return (
+		<main>
+			<SidebarWithHeader>
+				<Flex direction="column" gap={4} bg={'white'} p={4}>
+					<Accordion allowMultiple>
+						{changes.content?.map((change) => (
+							<AccordionChange
+								key={change.id}
+								productChange={change as ProductChanges}
+							/>
+						))}
+					</Accordion>
+					<Pagination
+						total={changes.totalElements}
+						colorScheme={'orange'}
+						perPage={changes.pageSize}
+					/>
+				</Flex>
+			</SidebarWithHeader>
+		</main>
+	);
+}

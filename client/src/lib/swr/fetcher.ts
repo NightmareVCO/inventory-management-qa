cd4586a1aa02c9d4bf422db24df55a4c17536440
NightmareVCO <vladimircuriel@outlook.com>
2025-08-20 12:15:41 -0400

    feat: add logging for request options in fetcher function

--- client/src/lib/swr/fetcher.ts
+++ client/src/lib/swr/fetcher.ts
@@ -6,6 +6,8 @@ export const fetcher = (url: string, options: RequestInit = {}) => {
 	};
 
 	return fetch(url, { ...options, headers }).then((res) => {
+		console.log('Request Options:', res);
+
 		if (!res.ok) {
 			throw new Error(`HTTP error! status: ${res.status}`);
 		}

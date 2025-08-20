6a022cf38bc053209a5f48bbab17013d9205bf29
NightmareVCO <vladimircuriel@outlook.com>
2025-08-20 12:14:03 -0400

    feat: update Keycloak configuration to use public environment variables

--- client/src/lib/constants/config.constants.ts
+++ client/src/lib/constants/config.constants.ts
@@ -1,18 +1,10 @@
-export const NEXT_API_URL =
-	process.env.NEXT_API_URL ?? 'http://localhost:8080/api/v1';
+export const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export const NEXT_PUBLIC_API_URL =
-	process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:8080/api/v1';
+export const NEXT_KEYCLOAK_URL = process.env.NEXT_PUBLIC_KEYCLOAK_URL;
 
-export const NEXT_KEYCLOAK_URL =
-	process.env.NEXT_KEYCLOAK_URL ?? 'http://localhost:7080';
+export const NEXT_KEYCLOAK_AUTH_URL = process.env.NEXT_PUBLIC_KEYCLOAK_AUTH_URL;
 
-export const NEXT_KEYCLOAK_AUTH_URL: string =
-	process.env.NEXT_KEYCLOAK_AUTH_URL ??
-	'http://localhost:7080/realms/inventory-realm/account';
-
-export const NEXT_KEYCLOAK_REALM =
-	process.env.NEXT_KEYCLOAK_REALM ?? 'inventory-realm';
+export const NEXT_KEYCLOAK_REALM = process.env.NEXT_PUBLIC_KEYCLOAK_REALM;
 
 export const NEXT_KEYCLOAK_CLIENT_ID =
-	process.env.NEXT_KEYCLOAK_CLIENT_ID ?? 'inventory-frontend';
+	process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID;

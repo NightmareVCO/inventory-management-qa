c6ff50fe515338f09d7ee1641424cd25b39710ad
NightmareVCO <vladimircuriel@outlook.com>
2025-08-21 01:39:04 -0400

    fix: format ProductsReportDTO for consistency

--- client/src/lib/model/dto/ProductsReports.dto.ts
+++ client/src/lib/model/dto/ProductsReports.dto.ts
@@ -1,9 +1,9 @@
 import type { Product } from '@lib/model/product.model';
 
 export type ProductsReportDTO = {
-  totalProductsWithMinStock: number;
-  totalProductsAddedLastWeek: number;
-  totalProductsDeletedLastWeek: number;
-  topProductsWithMostModifications: Product[];
-  valueOfDispatchedProductsByCategory: Record<string, number>;
+	totalProductsWithMinStock: number;
+	totalProductsAddedLastWeek: number;
+	totalProductsDeletedLastWeek: number;
+	topProductsWithMostModifications: Product[];
+	valueOfDispatchedProductsByCategory: Record<string, number>;
 };
